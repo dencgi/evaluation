0bbc17ae60f4f29c2d1480824c3ebe109ca328a6
depeckerd <denis.depecker@cgi.com>
2017-07-12 15:00:54 +0200

    Formatage.

--- web_service/src/test/java/dad/evaluation/controller/FibonacciControllerTest.java
+++ web_service/src/test/java/dad/evaluation/controller/FibonacciControllerTest.java
@@ -22,38 +22,60 @@ public class FibonacciControllerTest {
 
 	@Test
 	public void testServiceLinearOn() throws Exception {
-		mock.perform(get("/linear")).andExpect(status().isBadRequest());
+		mock.perform(get("/linear"))
+				.andExpect(status().isBadRequest());
 	}
 
 	@Test
 	public void testServiceReccusirveOn() throws Exception {
-		mock.perform(get("/reccursive")).andExpect(status().isBadRequest());
+		mock.perform(get("/reccursive"))
+				.andExpect(status().isBadRequest());
 	}
 
 	@Test
 	public void testServiceLinearValue1() throws Exception {
-		mock.perform(get("/linear?i=1")).andExpect(status().isOk()).andExpect(content().string("1"));
+		mock.perform(get("/linear?i=1"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("1"));
 	}
 
 	@Test
 	public void testServiceLinearMultipleValue() throws Exception {
-		mock.perform(get("/linear?i=2")).andExpect(status().isOk()).andExpect(content().string("1"));
-		mock.perform(get("/linear?i=7")).andExpect(status().isOk()).andExpect(content().string("13"));
-		mock.perform(get("/linear?i=11")).andExpect(status().isOk()).andExpect(content().string("89"));
-		mock.perform(get("/linear?i=25")).andExpect(status().isOk()).andExpect(content().string("75025"));
+		mock.perform(get("/linear?i=2"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("1"));
+		mock.perform(get("/linear?i=7"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("13"));
+		mock.perform(get("/linear?i=11"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("89"));
+		mock.perform(get("/linear?i=25"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("75025"));
 	}
 
 	@Test
 	public void testServiceReccursiveValue1() throws Exception {
-		mock.perform(get("/reccursive?i=1")).andExpect(status().isOk()).andExpect(content().string("1"));
+		mock.perform(get("/reccursive?i=1"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("1"));
 	}
 
 	@Test
 	public void testServiceReccursiveMultipleValue() throws Exception {
-		mock.perform(get("/reccursive?i=2")).andExpect(status().isOk()).andExpect(content().string("1"));
-		mock.perform(get("/reccursive?i=7")).andExpect(status().isOk()).andExpect(content().string("13"));
-		mock.perform(get("/reccursive?i=11")).andExpect(status().isOk()).andExpect(content().string("89"));
-		mock.perform(get("/reccursive?i=25")).andExpect(status().isOk()).andExpect(content().string("75025"));
+		mock.perform(get("/reccursive?i=2"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("1"));
+		mock.perform(get("/reccursive?i=7"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("13"));
+		mock.perform(get("/reccursive?i=11"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("89"));
+		mock.perform(get("/reccursive?i=25"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("75025"));
 	}
 
 }
